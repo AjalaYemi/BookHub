9e0fcf817c031213df0ef7202f485469bceb525a
AjalaYemi <opsyaj@yahoo.com>
2015-10-03 05:49:46 +0100

    Added MiniMagick for image resize

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,7 +4,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  #include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -32,10 +32,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # process resize_to_fit: [800,800]
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  
+  version :thumb do
+    process :resize_to_fit => [200, 200]
+  end
+  process resize_to_fill: [800, 800]
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
